2063242c96e1c9c1c22e18f5e4fc231e969ec9e3
burt <sralrobotics@gmail.com>
2019-07-29 18:10:36 -0400

    Pause Button
    Game is fully frozen upon pause
    
    FIX:  add a pause menu and allow edits to gravity, etc. during pause

---
+++ Assets/Scripts/PausePlay.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PausePlay : MonoBehaviour {
+
+	bool isPaused;
+	public Text pause;
+
+	// Use this for initialization
+	void Start () {
+		isPaused = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown ("space") && !isPaused) {
+			Time.timeScale = 0.0f;
+			isPaused = true;
+			pause.enabled = true;
+		} else if (Input.GetKeyDown ("space") && isPaused) {
+			Time.timeScale = 1.0f;
+			isPaused = false;
+			pause.enabled = false;
+		}
+	}
+}
